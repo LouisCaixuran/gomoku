430c1b207ac7aaeb21561db0559a473b994cbec4
caihui <cai.hui@zte.com.cn>
2019-12-02 01:16:06 -0800

    Refactory mcts code

--- gomoku/mcts.py
+++ gomoku/mcts.py
@@ -8,14 +8,14 @@ def rollout_policy_fn(board):
     action_probs = np.random.rand(len(board.available))
     return zip(board.available, action_probs)
 
-
 def policy_value_fn(board):
-    """a function that takes in a board and outputs a list of (action, probability)
-    tuples and a score for the board"""
+    """a function that takes in a board and outputs a list of (action, probability) tuples and a score for the board"""
     # return uniform probabilities and 0 score for pure MCTS
-    action_probs = np.ones(len(board.available))/len(board.available)
-    return zip(board.available, action_probs), 0
-
+    probs = np.ones(len(board.available))/len(board.available)
+    # probs = np.random.random(len(board.available))
+    # max_action = board.available[int(np.argwhere(probs==np.max(probs)))] #取最大值对应的下标值
+    action_probs =  zip(board.available,probs)
+    return action_probs,0   
 
 class TreeNode(object):
     """A node in the MCTS tree. Each node keeps track of its own value Q,
@@ -24,61 +24,23 @@ class TreeNode(object):
 
     def __init__(self, parent, prior_p):
         self._parent = parent
-        self._children = {}  # a map from action to TreeNode
+        self._children = {}  # dict action:TreeNode
         self._n_visits = 0
         self._Q = 0
         self._u = 0
         self._P = prior_p
 
-    def expand(self, action_priors):
-        """Expand tree by creating new children.
-        action_priors: a list of tuples of actions and their prior probability
-            according to the policy function.
-        """
-        for action, prob in action_priors:
-            if action not in self._children:
-                self._children[action] = TreeNode(self, prob)
-
-    def select(self, c_puct):
-        """Select action among children that gives maximum action value Q
-        plus bonus u(P).
-        Return: A tuple of (action, next_node)
-        """
-        return max(self._children.items(),
-                   key=lambda act_node: act_node[1].get_value(c_puct))
-
     def update(self, leaf_value):
-        """Update node values from leaf evaluation.
-        leaf_value: the value of subtree evaluation from the current player's
-            perspective.
-        """
         # Count visit.
         self._n_visits += 1
         # Update Q, a running average of values for all visits.
-        self._Q += 1.0*(leaf_value - self._Q) / self._n_visits
-
-    def update_recursive(self, leaf_value):
-        """Like a call to update(), but applied recursively for all ancestors.
-        """
-        # If it is not root, this node's parent should be updated first.
-        if self._parent:
-            self._parent.update_recursive(-leaf_value)
-        self.update(leaf_value)
+        self._Q = self._Q+1.0*(leaf_value-self._Q)/self._n_visits
 
     def get_value(self, c_puct):
-        """Calculate and return the value for this node.
-        It is a combination of leaf evaluations Q, and this node's prior
-        adjusted for its visit count, u.
-        c_puct: a number in (0, inf) controlling the relative impact of
-            value Q, and prior probability P, on this node's score.
-        """
-        self._u = (c_puct * self._P *
-                   np.sqrt(np.log(self._parent._n_visits)/(1+self._n_visits)))
+        self._u = self._P*c_puct * np.sqrt(np.log(self._parent._n_visits)/(0.1+self._n_visits))
         return self._Q + self._u
 
     def is_leaf(self):
-        """Check if leaf node (i.e. no nodes below this have been expanded).
-        """
         return self._children == {}
 
     def is_root(self):
@@ -88,56 +50,40 @@ class TreeNode(object):
 class MCTS(object):
     """A simple implementation of Monte Carlo Tree Search."""
 
-    def __init__(self, policy_value_fn, c_puct=5, simulate_time=100):
+    def __init__(self, policy_value_fn, c_puct=5, simulate_time=10):
         """
-        policy_value_fn: a function that takes in a board board and outputs
-            a list of (action, probability) tuples and also a score in [-1, 1]
-            (i.e. the expected value of the end game score from the current
-            player's perspective) for the current player.
-        c_puct: a number in (0, inf) that controls how quickly exploration
-            converges to the maximum-value policy. A higher value means
-            relying on the prior more.
+        policy_value_fn:策略函数
+        c_puct :控制深度优先，还是广度优先。参见UCB公式
+        simulate_time:每次模拟时间，单位是秒
         """
         self._root = TreeNode(None, 1.0)
+        self._root._n_visits = 0
         self._policy = policy_value_fn
         self._c_puct = c_puct
-        self.calculation_time = datetime.timedelta(seconds=simulate_time)
+        self.simulate_time = datetime.timedelta(seconds=simulate_time)
 
-
-    def _playout(self,board):
-        """Run a single playout from the root to the leaf, getting a value at
-        the leaf and propagating it back through its parents.
-        board is modified in-place, so a copy must be provided.
-        """
+    def _selection(self,board):
         node = self._root
-        while(1):
-            if node.is_leaf():
-
-                break
+        while not node.is_leaf():
             # Greedily select next move.
-            action, node = node.select(self._c_puct)
+            action, node = max(node._children.items(),
+                               key=lambda act_node: act_node[1].get_value(self._c_puct))
             board.set_action(action)
             board.next_states()
+        return board,node
 
-        action_probs, _ = self._policy(board)
-        # Check for end of game
-        end, winner = board.game_end()
-        if not end:
-            node.expand(action_probs)
+    def _expansion(self,board,node):
+        action_probs,_= self._policy(board)
+        #extand all children node
+        for action,probs in action_probs:
+            if action not in node._children:
+                node._children[action] = TreeNode(node, probs)
+
+    def _simulation(self,board):
         # Evaluate the leaf node by random rollout
-        leaf_value = self._evaluate_rollout(board)
-        # Update value and visit count of nodes in this traversal.
-        node.update_recursive(-leaf_value)
-
-    def _evaluate_rollout(self, board, limit=1000):
-        """Use the rollout policy to play until the end of the game,
-        returning +1 if the current player wins, -1 if the opponent wins,
-        and 0 if it is a tie.
-        """
-        player = board.current_player
+        player = board.moved_player
         end, winner = board.game_end()
-        
-        while len(board.available)>0 and not end:
+        while len(board.available)>0 and end == False:
             action_probs = rollout_policy_fn(board)
             max_action = max(action_probs, key=itemgetter(1))[0]
             board.set_action(max_action)
@@ -145,33 +91,46 @@ class MCTS(object):
             if end:                 
                 break            
             board.next_states()
-
+        
         if winner == -1:  # tie
-            return 0
+            leaf_value = 0
         else:
-            return 1 if winner == player else -1
+            leaf_value = 1 if winner == player else -1
+       
+        return leaf_value
 
+    def _backpropagation(self,node,leaf_value): 
+        # Update value and visit count of nodes in this traversal.
+        node.update(leaf_value)
+        while node._parent :
+            node = node._parent
+            leaf_value = -leaf_value
+            node.update(leaf_value)
+    
+    def _playout(self,board):
+        # Selection
+        board, node = self._selection(board)
+        # Expansion 
+        self._expansion(board,node)
+        # Simulation
+        leaf_value = self._simulation(board)
+        # Backpropagation 
+        self._backpropagation(node,leaf_value)
+    
     def get_move(self, board):
-        """Runs all playouts sequentially and returns the most visited action.
-        board: the current game board
-        Return: the selected action
-        """
+       
         begin = datetime.datetime.utcnow()
-        while datetime.datetime.utcnow() - begin < self.calculation_time:
+        while datetime.datetime.utcnow() - begin < self.simulate_time:
             board_copy = copy.deepcopy(board)
             self._playout(board_copy)
-        return max(self._root._children.items(),
-                   key=lambda act_node: act_node[1]._n_visits)[0]
 
-    def update_with_move(self, last_move):
-        """Step forward in the tree, keeping everything we already know
-        about the subtree.
-        """
-        if last_move in self._root._children:
-            self._root = self._root._children[last_move]
-            self._root._parent = None
-        else:
-            self._root = TreeNode(None, 1.0)
+        #选择访问次数最多的节点，不是最高胜率的节点，访问次数多结果更可信    
+        return max(self._root._children.items(),
+                key=lambda act_node: act_node[1]._n_visits)[0]
+
+
+    def update_with_move(self,move):
+        self._root = TreeNode(None, 1.0)
 
     def __str__(self):
         return "MCTS"
--- gomoku/tests/test_mcts.py
+++ gomoku/tests/test_mcts.py
@@ -3,7 +3,7 @@ import pytest
 from gomoku import board,player,mcts
 
 def test_mcts_get_move1():
-
+#本方有4连线
     chess = board.Gomoku(board_size=6)
     chess.last_action  = 1
     chess.moved_player = 2
@@ -19,38 +19,44 @@ def test_mcts_get_move1():
             chess.available.remove(action)
 
     chess.current_player = 1          
-    mcts_app = mcts.MCTS( mcts.policy_value_fn, c_puct=5, simulate_time=5)         
+    mcts_app = mcts.MCTS( mcts.policy_value_fn, c_puct=5, simulate_time=15)         
    
     count = 0
-    for i in range(4):
+    for i in range(3):
         move= mcts_app.get_move(chess)
         if move == 10:
             count = count + 1
 
-    assert count >= 2  #4次超过2次
+    assert count >= 2  #3次超过2次
 
-''' 
+
 def test_mcts_get_move2():
-
+#多方有4连线
     chess = board.Gomoku(board_size=6)
-    chess.last_action  = 1
-    chess.moved_player = 2
+    chess.last_action  = 7
+    chess.moved_player = 1
     chess.status =[ 
-              0,2,0,0,0,2,
+              2,2,0,0,0,2,
               0,1,1,1,0,0,
-              0,0,0,0,0,2,
-              2,0,0,0,0,0,
-              0,0,0,0,0,0,
-              0,0,0,0,0,0]
+              0,0,1,0,0,2,
+              2,0,0,1,0,0,
+              0,0,0,0,1,0,
+              2,2,0,0,0,0]
     for action in range(chess.size*chess.size):
         if chess.status[action] !=0:
             chess.available.remove(action)
 
-    chess.current_player = 1          
-    mcts_app = mcts.MCTS( mcts.policy_value_fn, c_puct=5, simulate_time=20)         
+    chess.current_player = 2          
+    mcts_app = mcts.MCTS( mcts.policy_value_fn, c_puct=5, simulate_time=15)         
 
     move= mcts_app.get_move(chess)
 
-    assert move==6 or move == 10
-'''    
+    count = 0
+    for i in range(3):
+        move= mcts_app.get_move(chess)
+        if move == 35:
+            count = count + 1
+
+    assert(move >= 2) 
+
 
