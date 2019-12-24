04b8e80ea43a301916c654be3bc9b0b95a435150
caihui <cai.hui@zte.com.cn>
2019-12-24 00:03:10 -0800

    update self_play test case

--- gomoku/tests/test_self_play.py
+++ gomoku/tests/test_self_play.py
@@ -10,7 +10,8 @@ class MockPlayer(object):
 
     def get_action(self, return_prob=0):
         probs= np.zeros(self.board.width*self.board.height)
-        probs[0]=1
+        #move = self.board.available
+        probs[0]=0.9
         move = 0
         return move, probs
 
@@ -19,32 +20,36 @@ class MockPlayer(object):
     
 
 def test_start_self_play():
-
     chess = board.Gomoku(board_size=2,n_long=1)
     player = MockPlayer(chess)
+    
     s=[]
     p=[]
     v=[]
+    
     state = np.zeros((4,2,2))
     probs = np.zeros((2*2))
-    value = 1
-    state[0][0,0]=1
-    state[2][0,0]=1
-    state[3]=1
-    probs[0]=1
+    
+    value=1.0
+    state[3]=1.0
+    
+    probs[0]=0.9
+
     s.append(state)
     p.append(probs)
     v.append(value)
     
-    result =list(zip(s,p,v))
+    result = list(zip(s,p,v))
 
     a = self_play.start_self_play(chess,player)
     
-    print(list(a)[0][2])
-    print(result[0][2])
-    assert result == list(a)
+    b=list(a)
 
-
+    for i in range(len(b)):
+        (b[i][0]==result[i][0]).all()
+        (b[i][1]==result[i][1]).all()
+        b[i][2]==result[i][2]
+        
      
 
 
